e3178330060efb1ef25350763319b494a1bd2524
Dainik Arthania <dnishchit@gmail.com>
2019-03-26 15:03:27 +0530

    schema remove

--- model/follow.js
+++ model/follow.js
@@ -5,11 +5,15 @@ sequelize=require("../config/database")
 let { STRING, INTEGER, DATE, literal, BOOLEAN } = Sequelize
 
     follow=sequelize.define('follow',{
+        // id:{type:Sequelize.INTEGER,primaryKey:true,autoIncrement:true},
+        // status:{type:INTEGER,allowNull:false},
+        // follows:{type:BOOLEAN,allowNull:false,defaultValue:false}
+        // },{
+        //   schema:'dainikgo'
+        // })
         id:{type:Sequelize.INTEGER,primaryKey:true,autoIncrement:true},
         status:{type:INTEGER,allowNull:false},
         follows:{type:BOOLEAN,allowNull:false,defaultValue:false}
-        },{
-          schema:'dainikgo'
         })
 
         module.exports=follow
