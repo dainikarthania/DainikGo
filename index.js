cfd1223c8853b150d2842f2df1670113de888cd8
Dainik Arthania <dnishchit@gmail.com>
2019-03-26 14:10:06 +0530

    fix

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ app=express()
 let session=require('express-session')
 let  appstart=require("./app")
 let path=require("path")
+const PORT = process.env.PORT || 5000
+
 app.use(session({  secret: 'DAINIKARTHANIA007))',
 }));
 app.use("/upload",express.static(path.join(__dirname, 'upload')));
@@ -11,7 +13,8 @@ appstart.register.register()
 appstart.home.home()
 appstart.followPage.follwpage()
 appstart.setting.private()
-app.listen(process.env.PORT ||3000, () => {
+app.listen(PORT, () => {
+console.log(PORT)
     console.log(`Server started on port`);
 });
 
