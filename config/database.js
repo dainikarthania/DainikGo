067d30314056890cb8612dd1797c06c02d5af6a2
Dainik Arthania <dnishchit@gmail.com>
2019-03-26 15:38:39 +0530

    fix

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ const Sequelize=require('sequelize')
 
 
 // module.exports= sequelize=new Sequelize('postgres://postgres:1234@localhost:5432/postgres')
-module.exports= sequelize=new Sequelize('postgres://hdhlgdudjrfcgc:9e803fb7fc5511ac7f81bc16a30406c0ae824098280929379f942a9980fdf8f7@ec2-184-73-216-48.compute-1.amazonaws.com:5432/dac97lqug728qg')
+module.exports= sequelize=new Sequelize('postgres://wkslwskgjhnxgl:70d6db33c047332089703c3eda77a35e426a85b9f2f4b868200018f459fa5065@ec2-184-73-216-48.compute-1.amazonaws.com:5432/dahg5pir178fd1')
 
