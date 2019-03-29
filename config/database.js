39886ede41afa02941152ce873615728426a8fe8
Dainik Arthania <dnishchit@gmail.com>
2019-03-29 11:06:47 +0530

    database elephant

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ const Sequelize=require('sequelize')
 
 
 // module.exports= sequelize=new Sequelize('postgres://postgres:1234@localhost:5432/postgres')
-module.exports= sequelize=new Sequelize('postgres://gecvtvslqjursd:feddf6d9cce355e2e54cd1e28207dd8491a60ee05aa5e3ca0d20d6e558deea91@ec2-54-221-236-144.compute-1.amazonaws.com:5432/dbc00hfjo9lu0g')
+module.exports= sequelize=new Sequelize('postgres://wnsrulnx:1UHa3KAhK2w9V9QiNRs52mKRqCKPHXfM@isilo.db.elephantsql.com:5432/wnsrulnx')
 
